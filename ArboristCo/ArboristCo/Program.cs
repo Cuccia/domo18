e613a0b4eab3847bb5d6811ce658fe92d90ff501
Anthony Cuccia <email@anthonycuccia.com>
2019-01-24 21:22:00 -0800

    Hello github!

--- ArboristCo/ArboristCo/Program.cs
+++ ArboristCo/ArboristCo/Program.cs
@@ -10,6 +10,8 @@ namespace ArboristCo
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello github!");
+            Console.ReadLine();
         }
     }
 }
